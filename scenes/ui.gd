c9f516c79dda3d0bd7e2af56ed783bd9f8a5e8fc
DiegoDuaS <diego.slowing@gmail.com>
2026-03-26 20:42:04 -0600

    feat: UI completed

--- scenes/ui.gd
+++ scenes/ui.gd
@@ -4,15 +4,33 @@ extends Control
 @onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var health_bar: ProgressBar = $HealthBar
 @onready var health_label: Label = $HealthLabel
+@onready var z_label = $TextureRect6
 
-func actualizar_icono(indice: int) -> void:
+@onready var indicadores_elemento = [
+	$TextureRect,  
+	$TextureRect2,
+	$TextureRect3, 
+	$TextureRect4  
+]
+
+func actualizar_icono(indice_actual: int) -> void:
 	if anim_sprite:
-		anim_sprite.frame = indice
+		anim_sprite.frame = indice_actual
 		var tween = create_tween()
 		tween.tween_property(anim_sprite, "scale", Vector2(1.2, 1.2), 0.1)
 		tween.tween_property(anim_sprite, "scale", Vector2(1.0, 1.0), 0.1)
-	else:
-		print("Error: No se encuentra el AnimatedSprite2D dentro del Control")
+
+	for i in range(indicadores_elemento.size()):
+		if i == indice_actual:
+			indicadores_elemento[i].visible = true  
+		else:
+			indicadores_elemento[i].visible = false 
+			
+	if z_label:
+		if indice_actual == 2:
+			z_label.visible = true
+		else:
+			z_label.visible = false
 
 func set_health(current: float, max_health: float) -> void:
 	if max_health <= 0:
